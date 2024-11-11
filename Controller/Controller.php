0dc444cd02eb640c1db8305a167639946c03b3ed
Roberto H Silva <robertosilva.sesisenai@gmail.comgit config --global user.namegit config --global user.email robertosilva.sesisenai@gmail.com>
2024-11-11 11:09:56 -0300

    Criação do Conteúdo do Controller e criação do config

---
+++ Controller/Controller.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'C:\aluno2\xampp\htdocs\atividaderefor\config.php';
+require_once 'C:\aluno2\xampp\htdocs\TRABALHO-2---Organizador-de-Viagens\Model\Model.php';
+
+class Controller
+{
+    private $Model;
+
+public function __construct($pdo){
+    $this->Model = new Model($pdo);
+}
+
+public function cadastrarviagem($nome,$data,$hora,$local,$roteiro){
+return $this->Model->cadastrarviagem($nome,$data,$hora,$local,$roteiro);
+}
+
+public function listarViagens(){
+    return $this->Model->listarViagens();
+}
+
+}
+?>
